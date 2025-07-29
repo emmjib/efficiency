0429a441c478c21c5d17cc88fddcbf126ea03d0f
emmjib <moviestash102@gmail.com>
2025-07-29 15:44:20 +0400

    Add files via upload

---
+++ time.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iterations = 0
+    print("The number entered by the user is:", n)
+    iterations += 1
+    print("The number of iterations is:", iterations)
+printnumber(4)
+printnumber(5)
+print("With any n, the time taken by the code will not change")
